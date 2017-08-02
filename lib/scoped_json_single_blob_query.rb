b9c9ee219a763292f69eaeee63e3288651f827f5
Phillip Gray <phillip@bignerdranch.com>
2017-08-02 17:28:42 -0600

    Add single company retrieval task

---
+++ lib/scoped_json_single_blob_query.rb
@@ -0,0 +1,8 @@
+class ScopedJsonSingleBlobQuery
+  def self.call
+    company = Financial.where(company: '10001', year: 2010)
+                            .select("data->1 AS data").first
+    data = company.data
+    data.sort_by { |_,v| v['value'].to_i }
+  end
+end
