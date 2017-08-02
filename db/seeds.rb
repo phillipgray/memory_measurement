ce52b61278b461abd7d89e3ee9fdca57fd75c9f8
Phillip Gray <phillip@bignerdranch.com>
2017-08-02 08:12:48 -0600

    Create seed file with sample data including data blob

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+seed_file = File.read("fin_data_sample2.json")
+data_blob = JSON.parse(seed_file)
+
+10001.upto(10010) do |company_name|
+  2000.upto(2010) do |year|
+    Financial.create(company: company_name.to_s, year: year, data: data_blob)
+  end
+end
