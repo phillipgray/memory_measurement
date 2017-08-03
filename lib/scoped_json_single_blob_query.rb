afe6cdc149751e7efa37a5b76496401625d5f08e
Phillip Gray <phillip@bignerdranch.com>
2017-08-03 09:04:13 -0600

    Use `p` method to simulate in-mem operations

--- lib/scoped_json_single_blob_query.rb
+++ lib/scoped_json_single_blob_query.rb
@@ -3,6 +3,7 @@ class ScopedJsonSingleBlobQuery
     company = Financial.where(company: '10001', year: 2010)
                             .select("data->1 AS data").first
     data = company.data
-    data.sort_by { |_,v| v['value'].to_i }
+    # data.sort_by { |_,v| v['value'].to_i }
+    p data
   end
 end
